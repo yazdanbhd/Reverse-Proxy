75972672b3d6ff9dd85fb0a94230053ff99fdf69
yazdanbhd <yzdanbhadry@gmail.com>
2024-12-06 14:31:35 +0330

    feat: finalize master.c with all components integrated

--- master.c
+++ master.c
@@ -1,26 +1,31 @@
 #include "master.h"
 
 pid_t workers[NUM_WORKERS];
+int current_worker = 0;
+
 int listen_fd_global;
 proxy_config_t config_global;
 
 void handle_sigchld(int sig) {
   pid_t pid;
   while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
-    printf("Worker with PID %d terminated. Restarting...\n", pid);
+
     for (int i = 0; i < NUM_WORKERS; i++) {
       if (workers[i] == pid) {
+        printf("Worker %d crashed. Restarting...\n", pid);
         pid_t new_pid = fork();
         if (new_pid == 0) {
-          printf("Restarted worker (PID: %d).\n", getpid());
-          // TODO: Worker logic
+
+          worker_main(listen_fd_global, &config_global, i);
           exit(0);
         } else if (new_pid > 0) {
           workers[i] = new_pid;
-          printf("Worker restarted with PID %d\n", new_pid);
+          printf("Worker %d restarted as %d\n", pid, new_pid);
         } else {
-          perror("Fork failed while restarting worker");
+          perror("Fork failed");
+          exit(EXIT_FAILURE);
         }
+        break;
       }
     }
   }
@@ -33,29 +38,29 @@ int create_listening_socket(const char *ip, int port) {
 
   if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("Socket creation failed");
-    return -1;
+    exit(EXIT_FAILURE);
   }
 
   if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-    perror("setsockopt failed");
-    return -1;
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
   }
 
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port);
   if (inet_pton(AF_INET, ip, &addr.sin_addr) <= 0) {
-    perror("Invalid IP address");
-    return -1;
+    perror("Invalid address");
+    exit(EXIT_FAILURE);
   }
 
   if (bind(listen_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
     perror("Bind failed");
-    return -1;
+    exit(EXIT_FAILURE);
   }
 
   if (listen(listen_fd, SOMAXCONN) < 0) {
     perror("Listen failed");
-    return -1;
+    exit(EXIT_FAILURE);
   }
 
   return listen_fd;
@@ -65,20 +70,15 @@ void fork_workers() {
   for (int i = 0; i < NUM_WORKERS; i++) {
     pid_t pid = fork();
     if (pid == 0) {
-      printf("Worker process forked (PID: %d).\n", getpid());
-      // TODO: Worker logic
+
+      worker_main(listen_fd_global, &config_global, i);
       exit(0);
     } else if (pid > 0) {
       workers[i] = pid;
       printf("Forked worker %d with PID %d\n", i, pid);
     } else {
       perror("Fork failed");
-      exit(1);
+      exit(EXIT_FAILURE);
     }
   }
-}
-
-int main(int argc, char *argv[]) {
-  printf("master process.\n");
-  return 0;
 }
